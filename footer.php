ebc95f91f14432135cd4c3586b860049729207c1
Jeff Janovetz <jjanovetz@sullivanbranding.com>
2012-07-30 14:27:20 -0500

    Initial footer

--- footer.php
+++ footer.php
@@ -3,6 +3,18 @@
 	</div><!-- #main -->
 
 	<footer>
+	<div style="height:56px;background-color:#424242;">
+		<div style="display:inline-table;margin-left:10px;">
+			<div style="display:table-cell;vertical-align:middle;height:56px;">
+				<a href="#" class="button">full site</a> <a href="tel:" class="button">call</a> <a href="mailto:" class="button">email</a>
+			</div>
+			<div style="display:table-cell;vertical-align:middle;height:56px;padding-left:10px;font-family:arial;font-size:10px;color:#fff;">
+				PathGroup Headquarters<br>
+				5301 Virginia Way<br>
+				Brentwood, TN 37027
+			</div>
+		</div>
+	</div>
 
 			
 	</footer>
