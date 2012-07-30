3ac809ef4787dbb9901b6e41dd7317e4ac1f9e6c
Jeff Janovetz <jjanovetz@sullivanbranding.com>
2012-07-30 14:30:27 -0500

    Committing all

--- header.php
+++ header.php
@@ -19,10 +19,10 @@
 			<div class="logo_box shadow_down">
 				<div id="header_logo"><a href="/"><img src="<?php bloginfo('template_directory'); ?>/images/pathgroup_logo.png" alt="pathgroup_logo" width="129" height="41" /></a></div>
 			</div>
-			<?php if (is_page("home")) { ?>
-				<div id="hero_image"><img src="<?php bloginfo('template_directory'); ?>/images/hero_image.jpg" alt="hero_image" width="320" height="78" /></div>
-			<?php } ?>
 		</div>
+		<?php if (is_page("home")) { ?>
+			<div id="hero_image"><img src="<?php bloginfo('template_directory'); ?>/images/hero_image.jpg" alt="hero_image" width="320" height="78" /></div>
+		<?php } ?>
 	</header>
 
 	<div id="main">
