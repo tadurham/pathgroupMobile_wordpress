545157ad6ed52b8442f7f76fa9dc94580122fddb
Jeff Janovetz <jjanovetz@sullivanbranding.com>
2012-07-30 12:58:10 -0500

    Completed header & styles

--- header.php
+++ header.php
@@ -14,7 +14,16 @@
 
 <body <?php body_class(); ?>>
 
-	<header></header>
+	<header>
+		<div style="margin-top:8px;" class="logo_box shadow_up">
+			<div class="logo_box shadow_down">
+				<div id="header_logo"><a href="/"><img src="<?php bloginfo('template_directory'); ?>/images/pathgroup_logo.png" alt="pathgroup_logo" width="129" height="41" /></a></div>
+			</div>
+			<?php if (is_page("home")) { ?>
+				<div id="hero_image"><img src="<?php bloginfo('template_directory'); ?>/images/hero_image.jpg" alt="hero_image" width="320" height="78" /></div>
+			<?php } ?>
+		</div>
+	</header>
 
 	<div id="main">
 	
