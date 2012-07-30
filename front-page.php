f9df5c5baaa3bdea2be7bab6f4fdc74afcbb020a
Taylor Durham <tdurham@sullivanbranding.com>
2012-07-30 13:27:27 -0500

    category section handling, Primary navigation adjustment, added default loop.

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
-          <!-- PRIMARY NAV //-->
-          <nav id="nav-main" class="nav-collapse collapse" role="navigation">
+            <!-- PRIMARY NAV //-->
+            <nav id="nav-main" class="nav-collapse collapse" role="navigation">
             <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav')); ?>
-          </nav>
-
+            </nav>
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-
+    <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
+      <div id="main" class="span6" role="main">
+        <div class="page-header">
+          <h1><?php _e('Latest Posts', 'roots');?></h1>
+        </div>
+<?php
+global $wp_query;
+$args = array_merge( $wp_query->query, array( 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+query_posts( $args );
+         get_template_part('loop', 'index'); ?>
+      </div><!-- /#main -->
+    </div><!-- /#content -->
 <?php get_footer(); ?>
