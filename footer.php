a8c9c99ff0596228e910cf390a8bb74da0fcb98a
Taylor Durham <tdurham@sullivanbranding.com>
2012-07-31 15:00:38 -0500

    redevelop footer, style individual pages

--- footer.php
+++ footer.php
@@ -3,20 +3,21 @@
 	</div><!-- #main -->
 
 	<footer>
-	<div style="height:56px;background-color:#424242;">
-		<div style="display:inline-table;margin-left:8px;">
-			<div style="display:table-cell;vertical-align:middle;height:56px;">
-				<?php echo wpmp_switcher_link('desktop', 'full site'); ?> <a href="tel:" class="button">call</a> <a href="mailto:" class="button">email</a>
-			</div>
-			<div style="display:table-cell;vertical-align:middle;height:56px;padding-left:8px;font-family:arial;font-size:10px;color:#fff;">
-				PathGroup Headquarters<br>
-				5301 Virginia Way<br>
-				Brentwood, TN 37027
-			</div>
+		<div class="fullSite buttonWrap">
+			<?php echo wpmp_switcher_link('desktop', 'full site'); ?>
 		</div>
-	</div>
-
-			
+		<div class="telephone buttonWrap">
+			<a href="tel:" class="button">call</a>
+		</div>
+		<div class="email buttonWrap">
+			<a href="mailto:" class="button">email</a>
+		</div>
+		<div class="address">
+			PathGroup Headquarters<br>
+			5301 Virginia Way<br>
+			Brentwood, TN 37027
+		</div>
+		<div class="clearfix"></div>
 	</footer>
 
 <?php wp_footer(); ?>
