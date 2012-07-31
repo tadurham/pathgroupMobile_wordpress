5df43588153df4e40015c4725be9df80004c2c60
Taylor Durham <tdurham@sullivanbranding.com>
2012-07-31 11:41:36 -0500

    correct funny zoom on iphone rotation

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1" />
 
     <title></title>
 
