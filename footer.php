845434ad5f301adbed634a95d30009b6a79168b5
Taylor Durham <tdurham@sullivanbranding.com>
2012-08-02 11:15:32 -0500

    update telephone number, email address and physical address.

--- footer.php
+++ footer.php
@@ -7,15 +7,16 @@
 			<?php echo wpmp_switcher_link('desktop', 'full site'); ?>
 		</div>
 		<div class="telephone buttonWrap">
-			<a href="tel:" class="button">call</a>
+			<a href="tel:888-410-4618" class="button">call</a>
 		</div>
 		<div class="email buttonWrap">
-			<a href="mailto:" class="button">email</a>
+			<a href="mailto:marketing@pathgroup.com" class="button">email</a>
 		</div>
 		<div class="address">
+		  <a href="http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=5301+Virginia+Way,+Brentwood,+TN&aq=0&oq=5301+Virginia+Way,&sll=37.6,-95.665&sspn=59.405322,56.513672&vpsrc=6&t=h&ie=UTF8&hq=&hnear=5301+Virginia+Way,+Brentwood,+Tennessee+37027&ll=36.0321,-86.809394&spn=0.00535,0.003449&z=18">
 			PathGroup Headquarters<br>
 			5301 Virginia Way<br>
-			Brentwood, TN 37027
+			Brentwood, TN 37027</a>
 		</div>
 		<div class="clearfix"></div>
 	</footer>
